05ec1dc62c50d0df90f5c25a00651eaa234a27da
Camila <camila.rodriguez.aguila@gmail.com>
2023-10-04 07:57:59 -0700

    Cambios en Orden de Repositorio

--- Programación 1/Practicas en Clase/SopaDeLetras.java
+++ Programación 1/Practicas en Clase/SopaDeLetras.java
@@ -1,4 +1,4 @@
-public class Main {
+public class SopaDeLetras {
     public static void main(String[] args) {
         int filas = 10;
         int cols = 12;
--- Programación 1/Tareas Programadas/Tarea #1, Draftosaurus/Scripts/DraftosaurusPruebass.java
+++
@@ -1,7 +0,0 @@
-public class DraftosaurusPruebass {
-    public static void main (String [] args){
-        Run in = new Run();
-        in.run();
-
-    }
-}
